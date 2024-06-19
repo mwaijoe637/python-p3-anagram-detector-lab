cee8f9472f01b4aaee6ec35e8302a9e2a8153040
mwaijoe637 <brakejoe9@gmail.com>
2024-06-19 11:03:38 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        def is_anagram(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        return [word for word in word_list if is_anagram(self.word, word)]
+
+# Sample usage
+if __name__ == "__main__":
+    listen = Anagram("listen")
+    print(listen.match(['enlists', 'google', 'inlets', 'banana']))
+    # Output should be ['inlets']
